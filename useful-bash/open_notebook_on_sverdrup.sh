c1fe5ae08095875b51ad120a9f1c2ada09c4b160
Timothy Smith <timsmith204@utexas.edu>
2019-07-16 14:16:11 +0200

    Streamline jupyter notebooks (#2)
    * scripts for opening notebooks on sverdrup
    
    * more helpful comments
    
    * one more helpful comment

---
+++ useful-bash/open_notebook_on_sverdrup.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Run this on a compute node, then use the alias'd ssh function to 
+# connect to the notebook on your local machine (i.e. your laptop)
+
+# Please see 'connect_to_sverdrup_notebook.sh' for instructions on how to use this
+
+export port_number=8890
+
+# A script to get a notebook going on sverdrup compute node
+unset XDG_RUNTIME_DIR
+
+# get the internet connection
+export internet_connect=`ip address show eth0 | grep -Po '(?<=[inet]) (\d+\.\d+\.\d+\.\d+)' | sed 's: ::g'`
+
+echo 'internet connection is:' $internet_connect
+
+# open the notebook
+jupyter notebook --no-browser --port=$port_number --ip=$internet_connect
