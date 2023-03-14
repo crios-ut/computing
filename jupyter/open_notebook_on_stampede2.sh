9046b945ee883f52106fbbc48daf0de910b6c33e
JCam98 <campbelljustin989@gmail.com>
2023-03-14 13:53:25 -0500

    added bash scripts for jupyter connection on TACC

---
+++ jupyter/open_notebook_on_stampede2.sh
@@ -0,0 +1,16 @@
+#! /bin/bash                                                                    
+
+# Run this on a compute node, then use the alias'd ssh function to              
+# connect to the notebook on your local machine (i.e. your laptop)              
+
+# Please see 'connect_to_stampede2_notebook.sh' for instructions on how to use this 
+
+export port_number=8870 # Set arbitrary four digit port number
+
+# A script to get a notebook going on stampede2 compute node                     
+unset XDG_RUNTIME_DIR
+
+echo 'port numer is: ' $port_number
+
+# open the notebook                                                             
+jupyter notebook --no-browser --port=$port_number --ip=*
